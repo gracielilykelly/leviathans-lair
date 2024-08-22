28a5ea3f0e6f6d6a8073a939502cf5d3ef90ff2d
Grace <gracielily93@gmail.com>
2024-08-22 19:17:35 +0100

    setup boulder

--- js/boulder.js
+++ js/boulder.js
@@ -1,3 +1,48 @@
 class Boulder {
-  constructor() {}
+
+  constructor(){
+    this.size = Math.random(10, 35);
+    this.angle = Math.random(0, TWO_PI);
+    this.travelSpeed = Math.random(.1, .7);
+    this.asset = new Asset(this.size, this.angle, this.travelSpeed);
+  }
+
+  // getters
+   getSize() {
+    return this.size;
+  }
+
+   getTravelSpeed() {
+    return this.travelSpeed;
+  }
+
+   getAngle() {
+    return this.angle;
+  }
+
+  getAsset() {
+    return this.asset;
+  }
+
+  // setters
+  setSize( s) {
+    this.size = s;
+  }
+
+   setTravelSpeed( speed) {
+    this.travelSpeed = speed;
+  }
+
+   setAngle( a) {
+    this.angle = a;
+  }
+
+  // methods
+   render() {
+    // displays the boulder
+    stroke(87, 96, 111);
+    strokeWeight(4);
+    fill(206, 214, 224);
+    ellipse(this.asset.getXCord(), this.asset.getYCord(), this.size, this.size);
+  }
 }
